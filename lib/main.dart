85df0d964e5d68eb24850f7d44664848e827f671
rishigarg09 <rishigarg185@gmail.com>
2024-09-05 22:26:22 +0530

    added caching and added appbar

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:wyb_task/video_player_screen.dart';
 
 void main() {
@@ -10,9 +11,16 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: VideoPlayerScreen(),
+    return ScreenUtilInit(
+      designSize: const Size(360, 800), 
+      minTextAdapt: true,
+      splitScreenMode: true,
+      builder: (context, child) {
+        return MaterialApp(
+          debugShowCheckedModeBanner: false,
+          home: VideoPlayerScreen(),
+        );
+      },
     );
   }
 }
--- lib/video_player_screen.dart
+++ lib/video_player_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:video_player/video_player.dart';
+import 'package:wyb_task/video_cache_manager.dart';
 
 class VideoPlayerScreen extends StatefulWidget {
   const VideoPlayerScreen({super.key});
@@ -13,6 +14,7 @@ class _VideoPlayerScreenState extends State<VideoPlayerScreen> {
   VideoPlayerController? _controller;
   int _currentIndex = 0;
   bool _isTransitioning = false;
+  double _progress = 0.0;
 
   final List<String> _videoPaths = [
     'https://videos.pexels.com/video-files/5586712/5586712-hd_1080_1920_25fps.mp4',
@@ -38,14 +40,33 @@ class _VideoPlayerScreenState extends State<VideoPlayerScreen> {
       await _controller!.dispose();
     }
 
-    _controller =
-        VideoPlayerController.networkUrl(Uri.parse(_videoPaths[index]))
-          ..initialize().then((_) {
-            setState(() {
-              _isTransitioning = false;
-            });
-            _controller?.play();
-          });
+    final videoPath = _videoPaths[index];
+    final cacheManager = VideoCacheManager();
+    final file = await cacheManager.getSingleFile(videoPath);
+
+    _controller = VideoPlayerController.file(file)
+      ..initialize().then((_) {
+        setState(() {
+          _isTransitioning = false;
+          _updateProgress();
+        });
+        _controller?.play();
+      });
+
+    _controller?.addListener(() {
+      if (_controller!.value.isPlaying) {
+        _updateProgress();
+      }
+    });
+  }
+
+  void _updateProgress() {
+    if (_controller != null && _controller!.value.isInitialized) {
+      setState(() {
+        _progress = _controller!.value.position.inMilliseconds /
+            _controller!.value.duration.inMilliseconds;
+      });
+    }
   }
 
   @override
@@ -60,8 +81,8 @@ class _VideoPlayerScreenState extends State<VideoPlayerScreen> {
     return Scaffold(
       backgroundColor: Colors.black,
       appBar: AppBar(
-        toolbarHeight: 0,
-        backgroundColor: const Color.fromRGBO(158, 158, 158, 1),
+        shadowColor: const Color.fromRGBO(158, 158, 158, 0.1),
+        toolbarHeight: kTextHeightNone,
       ),
       body: Stack(
         children: [
@@ -94,6 +115,28 @@ class _VideoPlayerScreenState extends State<VideoPlayerScreen> {
               );
             },
           ),
+          // Progress Bar
+          Positioned(
+            top: 0,
+            left: 0,
+            right: 0,
+            child: LinearProgressIndicator(
+              value: _progress,
+              backgroundColor: Colors.black.withOpacity(0.5),
+              valueColor: const AlwaysStoppedAnimation<Color>(Colors.white),
+            ),
+          ),
+          // Profile Circle Icon
+          Positioned(
+            top: 30,
+            left: 20,
+            child: CircleAvatar(
+              radius: 20,
+              backgroundImage: AssetImage('assets/profile$_currentIndex.jpg'),
+              backgroundColor: Colors.transparent,
+            ),
+          ),
+          // Bottom Profile Indicators
           Align(
             alignment: Alignment.bottomCenter,
             child: Container(
@@ -137,3 +180,146 @@ class _VideoPlayerScreenState extends State<VideoPlayerScreen> {
     );
   }
 }
+
+
+
+
+// import 'package:flutter/material.dart';
+// import 'package:video_player/video_player.dart';
+
+// class VideoPlayerScreen extends StatefulWidget {
+//   const VideoPlayerScreen({super.key});
+
+//   @override
+//   State<VideoPlayerScreen> createState() => _VideoPlayerScreenState();
+// }
+
+// class _VideoPlayerScreenState extends State<VideoPlayerScreen> {
+//   late PageController _pageController;
+//   VideoPlayerController? _controller;
+//   int _currentIndex = 0;
+//   bool _isTransitioning = false;
+
+//   final List<String> _videoPaths = [
+//     'https://videos.pexels.com/video-files/5586712/5586712-hd_1080_1920_25fps.mp4',
+//     'https://videos.pexels.com/video-files/6077689/6077689-sd_360_640_25fps.mp4',
+//     'https://videos.pexels.com/video-files/2791956/2791956-sd_360_640_25fps.mp4',
+//     'https://videos.pexels.com/video-files/5445273/5445273-sd_360_640_25fps.mp4',
+//   ];
+
+//   @override
+//   void initState() {
+//     super.initState();
+//     _pageController = PageController(initialPage: _currentIndex);
+//     _initializeAndPlay(_currentIndex);
+//   }
+
+//   void _initializeAndPlay(int index) async {
+//     setState(() {
+//       _isTransitioning = true;
+//     });
+
+//     if (_controller != null) {
+//       await _controller!.pause();
+//       await _controller!.dispose();
+//     }
+
+//     _controller =
+//         VideoPlayerController.networkUrl(Uri.parse(_videoPaths[index]))
+//           ..initialize().then((_) {
+//             setState(() {
+//               _isTransitioning = false;
+//             });
+//             _controller?.play();
+//           });
+//   }
+
+//   @override
+//   void dispose() {
+//     _controller?.dispose();
+//     _pageController.dispose();
+//     super.dispose();
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       backgroundColor: Colors.black,
+//       appBar: AppBar(
+//         toolbarHeight: 0,
+//         backgroundColor: const Color.fromRGBO(158, 158, 158, 1),
+//       ),
+//       body: Stack(
+//         children: [
+//           PageView.builder(
+//             controller: _pageController,
+//             itemCount: _videoPaths.length,
+//             onPageChanged: (index) {
+//               if (_currentIndex != index) {
+//                 setState(() {
+//                   _currentIndex = index;
+//                 });
+//                 _initializeAndPlay(index);
+//               }
+//             },
+//             itemBuilder: (context, index) {
+//               return AnimatedOpacity(
+//                 opacity: _isTransitioning ? 0.0 : 1.0,
+//                 duration: const Duration(milliseconds: 500),
+//                 child: FittedBox(
+//                   fit: BoxFit.cover,
+//                   child: SizedBox(
+//                     width: _controller?.value.size.width ?? 0,
+//                     height: _controller?.value.size.height ?? 0,
+//                     child:
+//                         _controller != null && _controller!.value.isInitialized
+//                             ? VideoPlayer(_controller!)
+//                             : const Center(child: CircularProgressIndicator()),
+//                   ),
+//                 ),
+//               );
+//             },
+//           ),
+//           Align(
+//             alignment: Alignment.bottomCenter,
+//             child: Container(
+//               height: 100,
+//               padding: const EdgeInsets.only(bottom: 20),
+//               child: Row(
+//                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+//                 children: List.generate(_videoPaths.length, (index) {
+//                   return GestureDetector(
+//                     onTap: () {
+//                       _pageController.animateToPage(
+//                         index,
+//                         duration: const Duration(milliseconds: 300),
+//                         curve: Curves.easeInOut,
+//                       );
+//                       _initializeAndPlay(index);
+//                     },
+//                     child: AnimatedContainer(
+//                       duration: const Duration(milliseconds: 300),
+//                       curve: Curves.easeInOut,
+//                       width: _currentIndex == index ? 80.0 : 60.0,
+//                       height: _currentIndex == index ? 80.0 : 60.0,
+//                       child: CircleAvatar(
+//                         backgroundImage: AssetImage('assets/profile$index.jpg'),
+//                         child: _currentIndex == index
+//                             ? Container(
+//                                 decoration: const BoxDecoration(
+//                                   shape: BoxShape.circle,
+//                                 ),
+//                               )
+//                             : null,
+//                       ),
+//                     ),
+//                   );
+//                 }),
+//               ),
+//             ),
+//           ),
+//         ],
+//       ),
+//     );
+//   }
+// }
