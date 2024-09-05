0df95a69453bcd7b49d91ae5daa8e35ea45308d1
rishigarg09 <rishigarg185@gmail.com>
2024-09-05 22:38:25 +0530

    added profile circular icon on top left part of page

--- lib/video_player_screen.dart
+++ lib/video_player_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart'; // Import ScreenUtil
 import 'package:video_player/video_player.dart';
 import 'package:wyb_task/video_cache_manager.dart';
 
@@ -78,11 +79,14 @@ class _VideoPlayerScreenState extends State<VideoPlayerScreen> {
 
   @override
   Widget build(BuildContext context) {
+    // Initialize ScreenUtil
+    ScreenUtil.init(context);
+
     return Scaffold(
       backgroundColor: Colors.black,
       appBar: AppBar(
         shadowColor: const Color.fromRGBO(158, 158, 158, 0.1),
-        toolbarHeight: kTextHeightNone,
+        toolbarHeight: 0, // kTextHeightNone doesn't exist in newer versions
       ),
       body: Stack(
         children: [
@@ -104,8 +108,8 @@ class _VideoPlayerScreenState extends State<VideoPlayerScreen> {
                 child: FittedBox(
                   fit: BoxFit.cover,
                   child: SizedBox(
-                    width: _controller?.value.size.width ?? 0,
-                    height: _controller?.value.size.height ?? 0,
+                    width: ScreenUtil().screenWidth, // Using ScreenUtil for width
+                    height: ScreenUtil().screenHeight, // Using ScreenUtil for height
                     child:
                         _controller != null && _controller!.value.isInitialized
                             ? VideoPlayer(_controller!)
@@ -117,7 +121,7 @@ class _VideoPlayerScreenState extends State<VideoPlayerScreen> {
           ),
           // Progress Bar
           Positioned(
-            top: 0,
+            bottom: 40.h, // Use .h for height scaling
             left: 0,
             right: 0,
             child: LinearProgressIndicator(
@@ -128,10 +132,10 @@ class _VideoPlayerScreenState extends State<VideoPlayerScreen> {
           ),
           // Profile Circle Icon
           Positioned(
-            top: 30,
-            left: 20,
+            top: 30.h, // Use .h for vertical scaling
+            left: 20.w, // Use .w for horizontal scaling
             child: CircleAvatar(
-              radius: 20,
+              radius: 20.r, // Use .r for radius scaling
               backgroundImage: AssetImage('assets/profile$_currentIndex.jpg'),
               backgroundColor: Colors.transparent,
             ),
@@ -140,8 +144,8 @@ class _VideoPlayerScreenState extends State<VideoPlayerScreen> {
           Align(
             alignment: Alignment.bottomCenter,
             child: Container(
-              height: 100,
-              padding: const EdgeInsets.only(bottom: 20),
+              height: 100.h, // Adjusted height with ScreenUtil
+              padding: EdgeInsets.only(bottom: 20.h), // Use .h for padding scaling
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: List.generate(_videoPaths.length, (index) {
@@ -157,8 +161,8 @@ class _VideoPlayerScreenState extends State<VideoPlayerScreen> {
                     child: AnimatedContainer(
                       duration: const Duration(milliseconds: 300),
                       curve: Curves.easeInOut,
-                      width: _currentIndex == index ? 80.0 : 60.0,
-                      height: _currentIndex == index ? 80.0 : 60.0,
+                      width: _currentIndex == index ? 80.w : 60.w, // Use .w for width scaling
+                      height: _currentIndex == index ? 80.h : 60.h, // Use .h for height scaling
                       child: CircleAvatar(
                         backgroundImage: AssetImage('assets/profile$index.jpg'),
                         child: _currentIndex == index
@@ -180,146 +184,3 @@ class _VideoPlayerScreenState extends State<VideoPlayerScreen> {
     );
   }
 }
-
-
-
-
-// import 'package:flutter/material.dart';
-// import 'package:video_player/video_player.dart';
-
-// class VideoPlayerScreen extends StatefulWidget {
-//   const VideoPlayerScreen({super.key});
-
-//   @override
-//   State<VideoPlayerScreen> createState() => _VideoPlayerScreenState();
-// }
-
-// class _VideoPlayerScreenState extends State<VideoPlayerScreen> {
-//   late PageController _pageController;
-//   VideoPlayerController? _controller;
-//   int _currentIndex = 0;
-//   bool _isTransitioning = false;
-
-//   final List<String> _videoPaths = [
-//     'https://videos.pexels.com/video-files/5586712/5586712-hd_1080_1920_25fps.mp4',
-//     'https://videos.pexels.com/video-files/6077689/6077689-sd_360_640_25fps.mp4',
-//     'https://videos.pexels.com/video-files/2791956/2791956-sd_360_640_25fps.mp4',
-//     'https://videos.pexels.com/video-files/5445273/5445273-sd_360_640_25fps.mp4',
-//   ];
-
-//   @override
-//   void initState() {
-//     super.initState();
-//     _pageController = PageController(initialPage: _currentIndex);
-//     _initializeAndPlay(_currentIndex);
-//   }
-
-//   void _initializeAndPlay(int index) async {
-//     setState(() {
-//       _isTransitioning = true;
-//     });
-
-//     if (_controller != null) {
-//       await _controller!.pause();
-//       await _controller!.dispose();
-//     }
-
-//     _controller =
-//         VideoPlayerController.networkUrl(Uri.parse(_videoPaths[index]))
-//           ..initialize().then((_) {
-//             setState(() {
-//               _isTransitioning = false;
-//             });
-//             _controller?.play();
-//           });
-//   }
-
-//   @override
-//   void dispose() {
-//     _controller?.dispose();
-//     _pageController.dispose();
-//     super.dispose();
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       backgroundColor: Colors.black,
-//       appBar: AppBar(
-//         toolbarHeight: 0,
-//         backgroundColor: const Color.fromRGBO(158, 158, 158, 1),
-//       ),
-//       body: Stack(
-//         children: [
-//           PageView.builder(
-//             controller: _pageController,
-//             itemCount: _videoPaths.length,
-//             onPageChanged: (index) {
-//               if (_currentIndex != index) {
-//                 setState(() {
-//                   _currentIndex = index;
-//                 });
-//                 _initializeAndPlay(index);
-//               }
-//             },
-//             itemBuilder: (context, index) {
-//               return AnimatedOpacity(
-//                 opacity: _isTransitioning ? 0.0 : 1.0,
-//                 duration: const Duration(milliseconds: 500),
-//                 child: FittedBox(
-//                   fit: BoxFit.cover,
-//                   child: SizedBox(
-//                     width: _controller?.value.size.width ?? 0,
-//                     height: _controller?.value.size.height ?? 0,
-//                     child:
-//                         _controller != null && _controller!.value.isInitialized
-//                             ? VideoPlayer(_controller!)
-//                             : const Center(child: CircularProgressIndicator()),
-//                   ),
-//                 ),
-//               );
-//             },
-//           ),
-//           Align(
-//             alignment: Alignment.bottomCenter,
-//             child: Container(
-//               height: 100,
-//               padding: const EdgeInsets.only(bottom: 20),
-//               child: Row(
-//                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-//                 children: List.generate(_videoPaths.length, (index) {
-//                   return GestureDetector(
-//                     onTap: () {
-//                       _pageController.animateToPage(
-//                         index,
-//                         duration: const Duration(milliseconds: 300),
-//                         curve: Curves.easeInOut,
-//                       );
-//                       _initializeAndPlay(index);
-//                     },
-//                     child: AnimatedContainer(
-//                       duration: const Duration(milliseconds: 300),
-//                       curve: Curves.easeInOut,
-//                       width: _currentIndex == index ? 80.0 : 60.0,
-//                       height: _currentIndex == index ? 80.0 : 60.0,
-//                       child: CircleAvatar(
-//                         backgroundImage: AssetImage('assets/profile$index.jpg'),
-//                         child: _currentIndex == index
-//                             ? Container(
-//                                 decoration: const BoxDecoration(
-//                                   shape: BoxShape.circle,
-//                                 ),
-//                               )
-//                             : null,
-//                       ),
-//                     ),
-//                   );
-//                 }),
-//               ),
-//             ),
-//           ),
-//         ],
-//       ),
-//     );
-//   }
-// }
